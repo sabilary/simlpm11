99a729b7ac0ac3b5dad71a27dea459dc150ca423
sabilary <sabila_september@yahoo.co.id>
2015-01-15 02:44:44 +0700

    ChangeLog-1.0.0

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,7 +2,7 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	/*
 	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
@@ -11,4 +11,12 @@ return array(
 	'password' => '',
 	'charset' => 'utf8',
 	*/
+    'connectionString'  => 'mysql:host=localhost;dbname=simlpm6',
+    'emulatePrepare'    => true,
+    'username'          => 'root',
+    'password'          => '',
+    'charset'           => 'utf8',
+    // to enable SQL logging and that would dump PDO parameter without SQL look statement in the log file
+    'enableParamLogging' => true,
+    'enableProfiling'    => true,
 );
--- protected/config/main.php
+++ protected/config/main.php
@@ -7,37 +7,72 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'SIM LPM',
+    'theme'=>'adminlte',
 
-	// preloading 'log' component
-	'preload'=>array('log'),
 
-	// autoloading model and component classes
+	/* -------------------------------------------------------------------------
+     * PRELOAD
+     * preloading 'log' component
+     * ------------------------------------------------------------------------- */
+	'preload'=>array(
+        'log',
+    ),
+
+
+	/* -------------------------------------------------------------------------
+     * IMPORT
+     * autoloading model and component classes
+     * ------------------------------------------------------------------------- */
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+		'application.components.widgets.*',
+        'application.helpers.*', // CArray is for image extension, check image 
 	),
 
+
+	/* -------------------------------------------------------------------------
+     * MODULES
+     * ------------------------------------------------------------------------- */
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>false,
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
 	),
 
-	// application components
+    
+	/* -------------------------------------------------------------------------
+     * COMPONENTS
+     * application components
+     * ------------------------------------------------------------------------- */
 	'components'=>array(
+        
+        // (EXTENSION) ImageMagick configuration
+        'image'=>array(
+            'class'=>'application.extensions.image.CImageComponent',
+            // GD or ImageMagick
+            'driver'=>'GD',
+            // ImageMagick setup path
+            'params'=>array('directory'=>'/opt/local/bin'),
+        ),
 
+        // =====================================================================
 		'user'=>array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
+			'loginUrl'=>array('//sign_in'),
 		),
 
+        // =====================================================================
+		// database settings are configured in database.php
+		'db'=>require(dirname(__FILE__).'/database.php'),
+
+        // =====================================================================
 		// uncomment the following to enable URLs in path-format
 		/*
 		'urlManager'=>array(
@@ -50,34 +85,36 @@ return array(
 		),
 		*/
 
-		// database settings are configured in database.php
-		'db'=>require(dirname(__FILE__).'/database.php'),
-
+        // =====================================================================
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
 		),
 
+        // =====================================================================
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
 				array(
 					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
+					'levels'=>'error, warning, trace',
 				),
 				// uncomment the following to show log messages on web pages
-				/*
 				array(
 					'class'=>'CWebLogRoute',
+					'levels'=>'error, warning, info',
 				),
-				*/
 			),
 		),
 
 	),
 
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
+    
+	/* -------------------------------------------------------------------------
+     * PARAMS
+     * application-level parameters that can be accessed
+	 * using Yii::app()->params['paramName']
+     * ------------------------------------------------------------------------- */
 	'params'=>array(
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',
